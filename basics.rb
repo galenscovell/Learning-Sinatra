b0e20061e6e96c1d5b1b0f0a8bb46749bc766862
Galen Scovell <galen.scovell@gmail.com>
2014-11-07 18:25:03 -0800

    Initial Commit
    Tutorial Part One

---
+++ basics.rb
@@ -0,0 +1,54 @@
+
+# Singing with Sinatra Part One
+
+
+require 'sinatra'
+
+
+# Root page
+get '/' do
+  "Hello Sinatra!"
+end
+
+
+# About page
+get '/about' do
+  'A little about me.'
+end
+
+
+# Param page 
+get '/hello/:name' do
+  "Hello there, #{params[:name]}."
+end
+
+
+# Form Page
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said '#{params[:message]}'."
+end
+
+
+# Encrypt page
+get '/encrypt' do
+  erb :encrypt
+end
+
+post '/encrypt' do
+  params[:encrypt].reverse
+end
+
+# Decrypt page
+get '/decrypt/:encrypt' do
+  params[:encrypt].reverse
+end
+
+
+# 404 Page
+not_found do
+  halt 404, 'Page not found!'
+end
