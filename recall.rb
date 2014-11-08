dbb599c7076b9777519b5800736d45dd793faf3a
Galen Scovell <galen.scovell@gmail.com>
2014-11-08 11:53:47 -0800

    Finish Part Two
    Really getting the hang of this, should be a piece of cake to create my
    own once I'm through all these basics.

--- recall.rb
+++ recall.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'data_mapper'
 
 
-# Setup new database
+# Setup new database with SQLite3
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
 
 class Note
@@ -18,9 +18,68 @@ end
 DataMapper.finalize.auto_upgrade!
 
 
+# Homepage
 get '/' do
-  @notes = Note.all :order => :id.desc
-  @title = 'All Notes'
+  @notes = Note.all :order => :id.desc # All Note objects by id to ERB
+  @title = 'All Notes' # Web page title for ERB
   erb :home
+end
+
+
+# Create new Note object on homepage when post request is made
+post '/' do
+  n = Note.new
+  n.content = params[:content]
+  n.created_at = Time.now
+  n.updated_at = Time.now
+  n.save
+  redirect '/' # Return to homepage
+end
+
+
+# Edit Note
+get '/:id' do
+  @note = Note.get params[:id]
+  @title = "Edit note ##{params[:id]}"
+  erb :edit
+end
+
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+
+# Delete Note
+get '/:id/delete' do
+  @note = Note.get params[:id]
+  @title = "Confirm deletion of note ##{params[:id]}"
+  erb :delete
+end
+
+delete '/:id' do
+  n = Note.get params[:id]
+  n.destroy
+  redirect '/'
+end
+
+
+# Complete Note
+get '/:id/complete' do
+  n = Note.get params[:id]
+  n.complete = n.complete ? 0 : 1
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+
+# Error page
+not_found do
+  halt 404, "Page not found."
 end
 
