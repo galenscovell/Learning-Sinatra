b3b752f35b29dcc0d4ad31aa9e14dd3815955956
Galen Scovell <galen.scovell@gmail.com>
2014-11-10 15:37:04 -0800

    Finish Part Three
    And that's it for the basics.

--- recall.rb
+++ recall.rb
@@ -2,6 +2,13 @@
 require 'sinatra'
 require 'data_mapper'
 
+# Flash message dependencies
+require 'sinatra/flash'
+require 'sinatra/redirect_with_flash'
+
+enable :sessions
+use Rack::Flash, :sweep => true
+
 
 # Setup new database with SQLite3
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
@@ -17,11 +24,20 @@ end
 
 DataMapper.finalize.auto_upgrade!
 
+# Creates h() method via Rack to escape submitted content
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
 
 # Homepage
 get '/' do
   @notes = Note.all :order => :id.desc # All Note objects by id to ERB
   @title = 'All Notes' # Web page title for ERB
+  if @notes.empty?
+    flash[:error] = 'No notes found. Add your first below.'
+  end
   erb :home
 end
 
@@ -32,8 +48,11 @@ post '/' do
   n.content = params[:content]
   n.created_at = Time.now
   n.updated_at = Time.now
-  n.save
-  redirect '/' # Return to homepage
+  if n.save
+    redirect '/', :notice => 'Note created!'
+  else
+    redirect '/', :error => 'Failed to create note.'
+  end
 end
 
 
@@ -41,16 +60,26 @@ end
 get '/:id' do
   @note = Note.get params[:id]
   @title = "Edit note ##{params[:id]}"
-  erb :edit
+  if @note
+    erb :edit
+  else
+    redirect '/', :error => 'Unable to find note.'
+  end
 end
 
 put '/:id' do
   n = Note.get params[:id]
+  unless n
+    redirect '/', :error => 'Unable to find note.'
+  end
   n.content = params[:content]
   n.complete = params[:complete] ? 1 : 0
   n.updated_at = Time.now
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', :notice => 'Note updated.'
+  else
+    redirect '/', :error => 'Error updating note.'
+  end
 end
 
 
@@ -58,23 +87,36 @@ end
 get '/:id/delete' do
   @note = Note.get params[:id]
   @title = "Confirm deletion of note ##{params[:id]}"
-  erb :delete
+  if @note
+    erb :delete
+  else
+    redirect '/', :error => 'Unable to find note.'
+  end
 end
 
 delete '/:id' do
   n = Note.get params[:id]
-  n.destroy
-  redirect '/'
+  if n.destroy
+    redirect '/', :notice => 'Note deleted.'
+  else
+    redirect '/', :error => 'Unable to delete note.'
+  end
 end
 
 
 # Complete Note
 get '/:id/complete' do
   n = Note.get params[:id]
+  unless n
+    redirect '/', :error => 'Unable to find note.'
+  end
   n.complete = n.complete ? 0 : 1
   n.updated_at = Time.now
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', :notice => 'Note marked complete.'
+  else
+    redirect '/', :error => 'Error marking note as complete.'
+  end
 end
 
 
